dbac0176a941a613e3df0bd287628f88592af53c
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2025-10-02 13:27:28 +0200

    fix: parseCsv issues
    - fix: not correct line break
    - fix: not corretly trimming line columns

--- src/utils/parseCsv.ts
+++ src/utils/parseCsv.ts
@@ -3,11 +3,11 @@ import type { Task } from '../types/Task';
 export type CsvRow = Pick<Task, 'title' | 'dueDate' | 'completed'>;
 
 export function parseCsv(input: string): CsvRow[] {
-  const lines = input.split('\n').filter((l) => l.length > 0);
+  const lines = input.split('\\r\\n').filter((l) => l.length > 0);
   return lines.map((line) => {
-    const [title, due, completed] = line.split(',');
+    const [title, due, completed] = line.split(',').map((l) => l.trim());
     return {
-      title,
+      title: title,
       dueDate: due,
       completed: completed === 'true',
     };
